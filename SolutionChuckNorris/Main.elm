f7aac83083bf719b313269f5df5533a563a16344
WebDevFromScratch <mybettingstuff@gmail.com>
2016-10-10 21:48:49 +0200

    base for Chuck Norris joke app

---
+++ SolutionChuckNorris/Main.elm
@@ -0,0 +1,98 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+import Http
+import Task
+
+
+randomJoke : Cmd Msg
+randomJoke =
+    let
+        url =
+            "http://api.icndb.com/jokes/random"
+
+        task =
+            Http.getString url
+
+        cmd =
+            Task.perform Fail Joke task
+    in
+        cmd
+
+
+
+-- model
+
+
+type alias Model =
+    String
+
+
+initModel : Model
+initModel =
+    "Finding a joke..."
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, randomJoke )
+
+
+
+-- update
+
+
+type Msg
+    = Joke String
+    | Fail Http.Error
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Joke joke ->
+            -- this satisfies the type, but at the same time doesn't actually
+            -- initiate any more actions (since we don't want any here)
+            ( joke, Cmd.none )
+
+        Fail error ->
+            ( (toString error), Cmd.none )
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text model ]
+
+
+
+-- subscription
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- main : Program Never
+-- main =
+--     App.beginnerProgram
+--         { model = initModel
+--         , view = view
+--         , update = update
+--         }
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
